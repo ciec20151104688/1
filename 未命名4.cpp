938483aff55a63c2ed07a3ff7366e2613f0c1fa2
ciec20151104688 <564319886@qq.com>
2016-07-07 15:04:35 +0800

    Add files via upload

--- 未命名4.cpp
+++ 未命名4.cpp
@@ -5,7 +5,8 @@ int main()
 {
 	FILE *fp;
 	char *a;
-	int len;
+	int flen;
+	printf("a len=%d",sizeof(a));
 	fp=fopen("d:\\20151104688\\export.gpx","r+");
 	if((fp=fopen("d:\\20151104688\\export.gpx","r+"))==0)
 	{
@@ -13,11 +14,11 @@ int main()
 		return(-1);
 	}
 	fseek(fp,0,SEEK_END);
-	len=ftell(fp);
-	a=(char*)malloc(len);
-	fseek(fp,0,0);
-	fread(a,1,len,fp);
-	printf("%s\n",a);
-	free(a);
+	flen=ftell(fp); 
+	fseek(fp,0,SEEK_SET);
+	a=(char *)malloc(1*flen);
+	fread(a,1,flen,fp);
+	printf("len=%d   %c\n",flen,a[0]);
+	fclose(fp);
 	return 0;
 }
